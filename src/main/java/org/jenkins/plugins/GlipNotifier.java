c0495daef0c10fb2c8d560ecaa8b6cd826b662fd
Tyler Long <tyler4long@gmail.com>
2017-07-06 13:49:45 +0800

    Add GlipNotifierDescriptor

--- src/main/java/org/jenkins/plugins/GlipNotifier.java
+++ src/main/java/org/jenkins/plugins/GlipNotifier.java
@@ -3,6 +3,10 @@ package org.jenkins.plugins;
 import org.kohsuke.stapler.DataBoundConstructor;
 import hudson.tasks.Notifier;
 import hudson.tasks.BuildStepMonitor;
+import hudson.Extension;
+import hudson.tasks.BuildStepDescriptor;
+import hudson.tasks.Publisher;
+import hudson.model.AbstractProject;
 
 public class GlipNotifier extends Notifier {
     public String webHookUrl;
@@ -22,5 +26,24 @@ public class GlipNotifier extends Notifier {
     @Override
     public BuildStepMonitor getRequiredMonitorService() {
         return BuildStepMonitor.NONE;
+    }
+
+    @Override
+    public GlipNotifierDescriptor getDescriptor() {
+        return (GlipNotifierDescriptor) super.getDescriptor();
+    }
+
+    @Extension
+    public static class GlipNotifierDescriptor extends BuildStepDescriptor<Publisher> {
+
+        @Override
+        public boolean isApplicable(Class<? extends AbstractProject> jobType) {
+            return true;
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "Outbound WebHook notification";
+        }
     }
 }
